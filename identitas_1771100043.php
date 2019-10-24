a978fcaa4f2c4253832a8ea7f0ca5b8c6dd6f5cb
Sriwiyanto1771100043 <56536497+SriWiyanto@users.noreply.github.com>
2019-10-24 22:12:20 +0700

    Add files via upload

---
+++ identitas_1771100043.php
@@ -0,0 +1,15 @@
+<?php
+class HelloWorld{
+	var $myHelloWorld;
+function myPrint () {
+print ("Hello, Saya Antok <br>");
+print ("Saya Mahasiswa TI <br>");
+print ("Saya Kuliah Unwidha <br>");
+print ("Saya Kuliah Malam TI <br>");
+print ("Saya Belajar Programmer <br>");
+print ("Sedang Belajar  PBO <br>");
+}
+}
+$myHelloWorld = new HelloWorld();
+$myHelloWorld->myPrint();
+?>
