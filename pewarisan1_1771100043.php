a9f743632bbeeb76f821337be2ce8958374cb624
Sriwiyanto1771100043 <56536497+SriWiyanto@users.noreply.github.com>
2019-11-04 14:57:37 +0700

    Pewarisan_Anak Bapak

---
+++ pewarisan1_1771100043.php
@@ -0,0 +1,16 @@
+<?php
+/* pewarisan.php */
+class Bapak{
+var $nama="Bapak";
+function Bapak($n){
+$this->nama=$n;
+}
+function Hallo(){
+echo "Halo,saya = ".$this->nama."<br>";
+}
+}
+class Anak extends Bapak{
+}
+$test=new Anak("Anak dari Bapak");
+$test->Hallo();
+?>
