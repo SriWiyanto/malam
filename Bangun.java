3388791f012f9149355fbaaef7773a9526489a9a
Sriwiyanto1771100043 <56536497+SriWiyanto@users.noreply.github.com>
2019-11-25 19:15:31 +0700

    1771100043_Bangun_Segitiga_Method Static

---
+++ Bangun.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author SMK2
+ */
+class Bangun2 {
+static void luassegitiga(){
+     int alas = 8;
+     int tinggi = 4;
+     System.out.println("Luas Segitiga = " + ((alas*tinggi)/2));
+}
+}
+class Segitiga {
+public static void main(String[] args) {
+Bangun2.luassegitiga();
+}
+}    
